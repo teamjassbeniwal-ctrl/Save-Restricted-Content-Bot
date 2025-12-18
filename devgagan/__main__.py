8e1004b6dff130720bd5b16eb465855b510780eb
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2025-12-18 21:54:51 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -3,9 +3,9 @@
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
 # Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
+# GitHub: https://github.com/
+# Telegram: https://t.me/TeamJB_bot
+# YouTube: https://youtube.com/@jassbeniwaltech/
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
 # Version: 2.0.5
