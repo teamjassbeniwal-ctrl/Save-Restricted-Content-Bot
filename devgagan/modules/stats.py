170b2ed50a61a8d177ea1b1314aab43f448827f0
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2025-12-18 22:29:41 +0530

    Update stats.py

--- devgagan/modules/stats.py
+++ devgagan/modules/stats.py
@@ -2,10 +2,10 @@
 # File Name: stats.py
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
+# Author: Team JB
+# GitHub: https:/https://github.com/teamjassbeniwal-ctrl/github.com/
+# Telegram: https://t.me/TeamJB_bot
+# YouTube: https://youtube.com/@jassbeniwaltech/
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
 # Version: 2.0.5
